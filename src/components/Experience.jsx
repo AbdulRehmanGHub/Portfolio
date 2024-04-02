ed11b464dc3d24ad48ee6018767c00126208e909
Abdul Rehman <105493274+AbdulRehmanGHub@users.noreply.github.com>
2024-04-02 08:52:38 +0000

    updated some files

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,17 +1,17 @@
+
 import React from "react";
 
-import html from "../assets/skills/html.png"
-import css from "../assets/skills/css.png"
-import javascript from "../assets/skills/javascript.png"
-import tailwind from "../assets/skills/tailwind.png"
-import php from "../assets/skills/php.png"
-import reactImage from "../assets/skills/react.png"
-import node from "../assets/skills/node.png"
-import mysql from "../assets/skills/mysql.png"
-import github from "../assets/skills/github.png"
+import html from "../assets/skills/html.png";
+import css from "../assets/skills/css.png";
+import javascript from "../assets/skills/javascript.png";
+import tailwind from "../assets/skills/tailwind.png";
+import php from "../assets/skills/php.png";
+import reactImage from "../assets/skills/react.png";
+import node from "../assets/skills/node.png";
+import mysql from "../assets/skills/mysql.png";
+import github from "../assets/skills/github.png";
 
 const Experience = () => {
-
     const techs = [
         {
             id: 1,
@@ -55,7 +55,6 @@ const Experience = () => {
             title: 'MySQL',
             style: 'shadow-orange-400'
         },
-        ,
         {
             id: 8,
             src: node,
@@ -70,31 +69,24 @@ const Experience = () => {
         }
     ];
 
-  return (
-  <div
-  name="experience"
-  className=" bg-gradient-to-b from-gray-800 to-black w-full h-fit  pt-24">
-    
-    <div
-    className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
-        <div>
-            <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">Experience</p>
-            <p className="py-6">These are the technologies I've worked with</p>
-        </div>
-
-        <div className=" w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-8">
-                {techs.map(({id, src, title, style}) => (
-            <div key={id}
-                 className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
-                <img src={src} alt="" className="w-20 mx-auto" />
-                <p className="mt-4">{title}</p>
+    return (
+        <div name="experience" className="bg-gradient-to-b from-gray-800 to-black w-full h-fit  pt-24">
+            <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
+                <div>
+                    <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">Experience</p>
+                    <p className="py-6">These are the technologies I've worked with</p>
+                </div>
+                <div className=" w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-8">
+                    {techs.map(({ id, src, title, style }) => (
+                        <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+                            <img src={src} alt="" className="w-20 mx-auto" />
+                            <p className="mt-4">{title}</p>
+                        </div>
+                    ))}
+                </div>
             </div>
-                    ))
-                }
         </div>
-    </div>
-  </div>
-  );
+    );
 };
 
-export default Experience
+export default Experience;
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import num1 from '../assets/portfolio/tribute website.jpg'
 import num2 from '../assets/portfolio/github profile finder.jpg'
@@ -8,69 +9,68 @@ import num6 from '../assets/portfolio/digital clock.jpg'
 
 const Portfolio = () => {
 
-  const portfolios = [
-    {
-      id: 1,
-      src: num1,
-      demoLink: "https://abdulrehmanghub.github.io/Tribute-to-AQ-Khan__Web-dev-Project/",
-      codeLink: "https://github.com/AbdulRehmanGHub/Tribute-to-AQ-Khan__Web-dev-Project"
-    },
-    {
-      id: 2,
-      src: num2,
-      demoLink: "https://abdulrehmanghub.github.io/GitHub-Profile-Finder/",
-      codeLink: "https://github.com/AbdulRehmanGHub/GitHub-Profile-Finder"
-    },
-    {
-      id: 3,
-      src: num3,
-      demoLink: "https://abdulrehmanghub.github.io/Image-Slider/",
-      codeLink: "https://github.com/AbdulRehmanGHub/Image-Slider"
-    },
-    {
-      id: 4,
-      src: num4,
-      demoLink: "https://AbdulRehmanGHub.github.io/Auto-Color-Generator",
-      codeLink: "https://github.com/AbdulRehmanGHub/Auto-Color-Generator"
-    },
-    {
-      id: 5,
-      src: num5,
-      demoLink: "https://AbdulRehmanGHub.github.io/Job-Form",
-      codeLink: "https://github.com/AbdulRehmanGHub/Job-Form"
-    },
-    {
-      id: 6,
-      src: num6,
-      demoLink: "https://abdulrehmanghub.github.io/Digital-Cock__Web-Dev-Project/",
-      codeLink: "https://github.com/AbdulRehmanGHub/Digital-Cock__Web-Dev-Project"
-    },
-  ]
+    const portfolios = [
+        {
+            id: 1,
+            src: num1,
+            demoLink: "https://abdulrehmanghub.github.io/Tribute-to-AQ-Khan__Web-dev-Project/",
+            codeLink: "https://github.com/AbdulRehmanGHub/Tribute-to-AQ-Khan__Web-dev-Project"
+        },
+        {
+            id: 2,
+            src: num2,
+            demoLink: "https://abdulrehmanghub.github.io/GitHub-Profile-Finder/",
+            codeLink: "https://github.com/AbdulRehmanGHub/GitHub-Profile-Finder"
+        },
+        {
+            id: 3,
+            src: num3,
+            demoLink: "https://abdulrehmanghub.github.io/Image-Slider/",
+            codeLink: "https://github.com/AbdulRehmanGHub/Image-Slider"
+        },
+        {
+            id: 4,
+            src: num4,
+            demoLink: "https://AbdulRehmanGHub.github.io/Auto-Color-Generator",
+            codeLink: "https://github.com/AbdulRehmanGHub/Auto-Color-Generator"
+        },
+        {
+            id: 5,
+            src: num5,
+            demoLink: "https://AbdulRehmanGHub.github.io/Job-Form",
+            codeLink: "https://github.com/AbdulRehmanGHub/Job-Form"
+        },
+        {
+            id: 6,
+            src: num6,
+            demoLink: "https://abdulrehmanghub.github.io/Digital-Cock__Web-Dev-Project/",
+            codeLink: "https://github.com/AbdulRehmanGHub/Digital-Cock__Web-Dev-Project"
+        },
+    ];
 
-  return (
-    <div name="portfolio" className="bg-gradient-to-b from-black to-gray-800 w-fullk text-white md:h-fit">
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-5 mt-20">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">Portfolio</p>
-          <p className="py-6">Check out some of my work right here</p>
-        </div>
-
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-8">
-          {
-            portfolios.map(({id, src, demoLink, codeLink}) => (
-              <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
-                <img src={src} alt="" className="rounded-md duration-200 hover:scale-105" />
-                <div className="flex items-center justify-center">
-                  <a href={demoLink} target="_blank" rel="noopener noreferrer" className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Demo</a>
-                  <a href={codeLink} target="_blank" rel="noopener noreferrer" className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Code</a>
+    return (
+        <div name="portfolio" className="bg-gradient-to-b from-black to-gray-800 w-fullk text-white md:h-fit">
+            <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+                <div className="pb-5 mt-20">
+                    <p className="text-4xl font-bold inline border-b-4 border-gray-500">Portfolio</p>
+                    <p className="py-6">Check out some of my work right here</p>
                 </div>
-              </div>
-            ))
-          }
+                <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-8">
+                    {
+                        portfolios.map(({ id, src, demoLink, codeLink }) => (
+                            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+                                <img src={src} alt="" className="rounded-md duration-200 hover:scale-105" />
+                                <div className="flex items-center justify-center">
+                                    <a href={demoLink} target="_blank" rel="noopener noreferrer" className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Demo</a>
+                                    <a href={codeLink} target="_blank" rel="noopener noreferrer" className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Code</a>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Portfolio;
