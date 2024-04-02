53df983ed99afdbc4ab516fcf81f549297a19737
AbdulRehmanGHub <pakkhan9009@gmail.com>
2024-04-02 13:26:34 +0500

    portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-fit bg-gradient-to-b from-gray-800 to-black text-white">
+        
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full pt-20">
+            <div className="pb-8">
+                <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+            </div>
+
+            <p className="text-xl mt-6">
+                I am a Full Stack Web Developer having 2+ Years of experience in building websites and web applications using Modern Technologies. I have a strong foundation in web development and programming. I am passionate about learning new technologies and implementing them in real-world projects. I have a strong understanding of web development concepts and a keen interest in working on challenging projects. I am a quick learner and a team player who can work efficiently in a fast-paced environment.
+            </p>
+
+            <br/>
+
+            <p className="text-xl mt-6">
+                I have hands-on experience in Frontend Technologies like HTML5, CSS3, JavaScript, React, and more. I have also worked on Backend Technologies like PHP, Node JS, and MySQL. I have worked on various projects during my career and have developed a strong understanding of the software development life cycle. I have a strong understanding of version control systems like Git and GitHub. I have also worked on various projects using different APIs and libraries.
+            </p>
+        </div>
+
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Link, animateScroll as scroll } from "react-scroll";
+
+const NavBar = () => {
+    const [nav, setNav] = useState(false);
+
+    return (
+        <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+            <div>
+                <h1 className="text-5xl font-signature ml-2">Dev. Abdul Rehman</h1>
+            </div>
+
+            <ul className="hidden md:flex">
+                <NavItem to="home" text="Home" />
+                <NavItem to="about" text="About" />
+                <NavItem to="portfolio" text="Portfolio" />
+                <NavItem to="experience" text="Experience" />
+                <NavItem to="contact" text="Contact" />
+            </ul>
+
+            <div
+                onClick={() => setNav(!nav)}
+                className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden">
+                {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+            </div>
+
+            {nav && (
+                <ul className="flex flex-col justify-center items-center absolute top-0 left-9 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+                    <NavItem to="home" text="Home" closeNav={() => setNav(false)} />
+                    <NavItem to="about" text="About" closeNav={() => setNav(false)} />
+                    <NavItem to="portfolio" text="Portfolio" closeNav={() => setNav(false)} />
+                    <NavItem to="experience" text="Experience" closeNav={() => setNav(false)} />
+                    <NavItem to="contact" text="Contact" closeNav={() => setNav(false)} />
+                </ul>
+            )}
+        </div>
+    );
+};
+
+const NavItem = ({ to, text, closeNav }) => (
+    <li
+        onClick={closeNav}
+        className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200">
+        <Link
+            activeClass="active"
+            to={to}
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+        >
+            {text}
+        </Link>
+    </li>
+);
+
+export default NavBar;
